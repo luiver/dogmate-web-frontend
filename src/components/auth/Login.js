76acf1c446a6297855512d878f0d4f41d6a7eb4f
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-11-30 19:12:32 +0100

    add classes for html and error message

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import './login.css'
 
 export default class Login extends Component {
     constructor(props) {
@@ -42,20 +43,31 @@ export default class Login extends Component {
             })
             .catch(error => {
                 console.log("login error", error);
+                this.changeErrorMessage()
             })
         event.preventDefault();
     }
 
+    changeErrorMessage = () => {
+        const { loginErrors } = this.state;
+        return this.setState({
+            loginErrors: loginErrors + "Wrong Username or Password!"
+        })
+    };
+
     render() {
         return (
             //todo Header here
-            <div>
-                <h1>Login Page</h1>
+
+            <div className={"login-outer-box light-blue"}>
+                <div className={"login-inner-box"}>
+                <h1 className={"brown"}>Sign in</h1>
+                <h4>{this.state.loginErrors}</h4>
                 <form onSubmit={this.handleSubmit}>
                     <input
                         type="email"
                         name="email"
-                        placeholder="Email"
+                        placeholder="email"
                         value={this.state.email}
                         onChange={this.handleChange}
                         required
@@ -63,14 +75,17 @@ export default class Login extends Component {
                     <input
                         type="password"
                         name="password"
-                        placeholder="Password"
+                        placeholder="password"
                         value={this.state.password}
                         onChange={this.handleChange}
                         required
                     />
 
-                    <button type="submit">Sign in</button>
+                    <button className={"yellow"} type="submit">Sign in!</button>
                 </form>
+                </div>
+                <span className={"brown"}>Don't have an account? </span>
+                <a className={"redirect"} href={"/register"}>Sign up!</a>
             </div>
         );
     }
