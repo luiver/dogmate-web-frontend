7bc1c97a3c88ca31819b9f8692cc0729170d34ea
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-12-01 15:19:16 +0100

    add header

---
+++ src/components/header/Header.js
@@ -0,0 +1,19 @@
+import {Component} from "react";
+import './header.css'
+export default class Header extends Component {
+
+    render() {
+        return  (
+            <div className={"brown-background"}>
+                <header>
+                    <div className="logo">DOGMATE</div>
+                    <div className="main-avatar yellow">
+                        A
+                    </div>
+                </header>
+            </div>
+        )
+
+    }
+
+}
