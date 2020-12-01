3865fed06702322b56b378dcc2e97abfd03f117b
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-12-01 17:18:28 +0100

    add avatar show only for logged in user

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,17 +2,47 @@ import {Component} from "react";
 import './header.css'
 
 export default class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.changeIsLogin = this.changeIsLogin.bind(this);
+        this.state = {
+            isLogIn: true
+        };
+    }
+
+    changeIsLogin = () => {
+        this.setState(prevState => ({ isLogIn: !prevState.isLogIn }));
+    }
 
     render() {
+        const { isLogIn } = this.state;
+        let avatar;
+
+        if (isLogIn){
+            avatar = <UserLogo/>
+        }
+
         return  (
             <div className={"brown-background"}>
                 <header>
                     <div className="logo">DOGMATE</div>
-                    <div className="main-avatar yellow">
-                        <span>A</span>
-                    </div>
+                    {avatar}
                 </header>
             </div>
         )
     }
+
 }
+
+const UserLogo = props => {
+    return (
+        <div
+            className="main-avatar yellow"
+            onClick={props.onClick}
+        >
+            <span>
+                A
+            </span>
+        </div>
+    );
+};
