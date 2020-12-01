6a6fe71f20678a13a0e5beed4d28bbbc082b501c
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-12-01 21:55:31 +0100

    add callbacks handling used to check is user isLogIn and ifAvatarIsClicked

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,17 +4,30 @@ import './header.css'
 export default class Header extends Component {
     constructor(props) {
         super(props);
+        this.changeState = this.changeState.bind(this);
+        this.state = {
+            isLogoClicked: false,
+        };
+    }
+
+    changeState() {
+        this.setState(prevState => ({ isLogoClicked: !prevState.isLogoClicked  }));
+    }
+
+    changeIsAvatarClicked = (event) => {
+        this.changeState();
+        const {isLogoClicked} = this.state;
+        this.props.isAvatarClickedCallback(!isLogoClicked);
+        event.preventDefault();
     }
 
     render() {
-        const isLoginRef = this.props.isLoginRef
+        const isLoginRef = this.props.isLoginRef;
         return  (
-            <div className={"brown-background"}>
-                <header>
-                    <div className="logo">DOGMATE</div>
-                    {isLoginRef && <UserLogo/>}
-                </header>
-            </div>
+            <header>
+                <div className="logo">DOGMATE</div>
+                {isLoginRef && <UserLogo onClick={this.changeIsAvatarClicked.bind(this)}/>}
+            </header>
         )
     }
 }
--- src/components/template/MainTemplate.js
+++ src/components/template/MainTemplate.js
@@ -1,44 +1,51 @@
 import {Component} from "react";
-import '../../index.css'
 import Header from "../header/Header";
+import Login from "../auth/Login";
+import Home from "../home/Home";
+import MainNavigation from "../navigation/MainNavigation";
+import AvatarMenu from "../navigation/AvatarMenu";
+import '../../index.css'
+import './mainTemplate.css'
+import Registration from "../auth/Registration";
 
 export default class MainTemplate extends Component {
 
     constructor(props) {
         super(props);
-        this.changeIsLogin = this.changeIsLogin.bind(this);
         this.state = {
-            isLogIn: true,
+            isLogIn: false,
+            isAvatarClicked: null
         }
-
     }
 
-    changeIsLogin = () => {
-        this.setState(prevState => ({ isLogIn: !prevState.isLogIn }));
+    handleIsLoginCallback = (data) => {
+        this.setState({isLogIn: data});
+    }
+
+    handleIsAvatarClicked = (data) => {
+        this.setState({isAvatarClicked: data});
     }
 
     render() {
-
-        const { isLogIn } = this.state;
-
-        if (isLogIn) {
-
-        }
+        const doesUserWantToRegister = this.props.doesUserWantToRegister
+        const { isLogIn, isAvatarClicked } = this.state;
 
         return  (
             <div>
-                <div className="main-header">
-                    <Header isLoginRef={isLogIn}/>
+                <div className="main-header brown-background">
+                    <Header isLoginRef={isLogIn} isAvatarClickedCallback={this.handleIsAvatarClicked}/>
                 </div>
                 <div className="main-container">
                     <div className="left-menu">
-
+                        {isLogIn && <MainNavigation/>}
                     </div>
                     <div className="content-container">
-
+                        {isLogIn && <Home/>}
+                        {!isLogIn && !doesUserWantToRegister && <Login isLogInCallback={this.handleIsLoginCallback} />}
+                        {!isLogIn && doesUserWantToRegister && <Registration/>}
                     </div>
                     <div className="right-container">
-
+                        {isAvatarClicked && <AvatarMenu/>}
                     </div>
                 </div>
             </div>
