5e47f6cf6a25ae494501aad5151566d0846e10bf
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-12-01 21:51:19 +0100

    create basic classes for navigation menu and avatar menu with corresponding css files

---
+++ src/components/navigation/AvatarMenu.js
@@ -0,0 +1,16 @@
+import {Component} from "react";
+import './avatarMenu.css'
+
+export default class AvatarMenu extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                AVATAR MENU
+            </div>
+        )
+    }
+}
---
+++ src/components/navigation/MainNavigation.js
@@ -0,0 +1,16 @@
+import {Component} from "react";
+import './mainNavigation.css'
+
+export default class MainNavigation extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                MAIN NAVIGATION
+            </div>
+        )
+    }
+}
